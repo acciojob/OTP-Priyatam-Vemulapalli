9409b125e62a5447713da2a892b88560793464d3
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-17 21:09:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const codes = document.querySelectorAll('.code');
 
-codes.forEach((code, id) => {
+codes.forEach((code, idx) => {
     code.addEventListener('input', () => {
-        if (code.value.length === 1 && id < codes.length - 1) {
-            codes[id + 1].focus();
+        if (code.value.length === 1 && idx < codes.length - 1) {
+            codes[idx + 1].focus();  // Move to the next input
         }
     });
 
     code.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && code.value.length === 0 && id > 0) {
-            codes[id - 1].focus();
+        if (e.key === 'Backspace' && code.value.length === 0 && idx > 0) {
+            codes[idx - 1].focus();  // Move to the previous input
         }
     });
 });
