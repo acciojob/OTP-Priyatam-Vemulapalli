07a39a40f41a2e6bebdfabb68b9907ab5bf3528a
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-17 20:53:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes.forEach((code, id) => {
+    code.addEventListener('input', () => {
+        if (code.value.length === 1 && id < codes.length - 1) {
+            codes[id + 1].focus();
+        }
+    });
+
+    code.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && code.value.length === 0 && id > 0) {
+            codes[id - 1].focus();
+        }
+    });
+});
